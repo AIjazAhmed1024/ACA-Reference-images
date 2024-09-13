fac8c683b88dd3e6502405c7be2d85edb2d32476
Mohammed Aijaz Ahmed <136674350+AIjazAhmed1024@users.noreply.github.com>
2024-09-13 12:04:17 +0530

    S2S

---
+++ S2S/assets/js/script.js
@@ -0,0 +1,29 @@
+$(function() {
+    "use strict";
+
+    /*-----------------------------------
+     * FIXED  MENU - HEADER
+     *-----------------------------------*/
+    function menuscroll() {
+        var $navmenu = $('.header');
+        if ($(window).scrollTop() > 50) {
+            $navmenu.addClass('is-scrolling');
+        } else {
+            $navmenu.removeClass("is-scrolling");
+        }
+    }
+    menuscroll();
+    $(window).on('scroll', function() {
+        menuscroll();
+    });
+    /*-----------------------------------
+     * NAVBAR CLOSE ON CLICK
+     *-----------------------------------*/
+
+    $('.navbar-nav > li:not(.dropdown) > a').on('click', function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+ 
+
+
+});
